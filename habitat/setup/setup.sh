6d5d45f58b8161a378350688ff298621a5dfc6b0
root <montyrhodes8@gmail.com>
2021-12-23 19:58:17 +0000

    Added in setup files for habitat

--- habitat/setup/setup.sh
+++ habitat/setup/setup.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
+echo "RUNNING SETUP FOR habitat"
+
+
+# Moving /etc files
+
+mv hosts /etc/hosts
+
+mv hostname /etc/hostname
+
+mv fstab /etc/fstab
+
+
+# Configuring network settings
+echo "Configuring static IP address at 192.168.0.8"
+
+mv 01-netcfg.yaml /etc/netplan/01-netcfg.yaml
+
 
