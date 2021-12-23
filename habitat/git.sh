1a432aa9c2c16fefa8276952a3524bc5a83da9c5
root <montyrhodes8@gmail.com>
2021-12-23 20:33:31 +0000

    git.sh push working

--- habitat/git.sh
+++ habitat/git.sh
@@ -4,12 +4,17 @@
 # Created: 23/12/2021
 # This script will either setup ssh keys or push
 
-read -p "Are you setting up ssh or pushing? " $option
+read -p "Are you setting up ssh or pushing? " option
 
-if [ "$option" = "ssh" ]; then
+if [[ $option == "ssh" ]]; then
 
+	echo "Configuring ssh keys ..."
 	ssh-keygen -t ed25519 -C "montyrhodes8@gmail.com"
 
-elif [ "$option" = "push" ]; then
+elif [[ $option == "push" ]]; then
+
+	echo "Pushing ..."
 	git add .
+	git commit -a
+	git push
 fi
